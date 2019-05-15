8acc1e3e26c16c29d83ac471861fc1d99d0016a9
AndrewYinLi <ali@oberlin.edu>
2019-05-15 01:21:57 -0700

    Testing db

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+
+	bolt "go.etcd.io/bbolt"
+)
+
 func main() {
-	
+	// Open the my.db data file in your current directory.
+	// It will be created if it doesn't exist.
+	db, err := bolt.Open("my.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 }
