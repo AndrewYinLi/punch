f57669c2d95a98be50e53768f7daf29b6a4cf337
AndrewYinLi <ali@oberlin.edu>
2019-05-15 19:05:07 -0700

    Basic functionality

--- main.go
+++ main.go
@@ -1,17 +1,54 @@
 package main
 
 import (
-	"log"
-
+	"fmt"
 	bolt "go.etcd.io/bbolt"
+	"log"
+	"strings"
+	"time"
 )
 
-func main() {
-	// Open the my.db data file in your current directory.
-	// It will be created if it doesn't exist.
-	db, err := bolt.Open("my.db", 0600, nil)
+// Record time in
+func punch(){
+	// Open `hours.db` in cd. Creates the database if it doesn't exist.
+	db, err := bolt.Open("hours.db", 0600, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
+	err = db.Update(func(tx *bolt.Tx) error {
+		// Create bucket to hold today's in and out time if it doesn't exist
+		logBucket, err := tx.CreateBucketIfNotExists([]byte("log"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		timeDate := time.Now().Format("2006-01-02 15:04")
+		timeDateSplit := strings.Split(timeDate, " ")
+		currentTime := timeDateSplit[1]
+		currentDate :=  timeDateSplit[0] // Use today's date as key
+		hoursWorked := logBucket.Get([]byte(currentDate)) // Value from bucket is hours worked
+		if(hoursWorked == nil) { // Punch in because no time has been logged for today
+			err := logBucket.Put([]byte(currentDate), []byte(currentTime))
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println("Punched in at", currentTime)
+		} else{ // Punch out because time in was previously logged
+			err := logBucket.Put([]byte(currentDate), append(logBucket.Get([]byte(currentDate)), append([]byte(" "), []byte(currentTime)...)...))
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println("Punched out at", currentTime)
+		}
+
+		return nil
+	})
+	if err != nil {
+		log.Fatal("Error: Could not log hours in database.")
+	}
+}
+
+func main() {
+	punch()
+
 }
