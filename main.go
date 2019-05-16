98234e13dd14a8baa66765f9672b07a351fa6bac
AndrewYinLi <ali@oberlin.edu>
2019-05-15 21:10:26 -0700

    Log hours worked per day bug fix

--- main.go
+++ main.go
@@ -4,9 +4,43 @@ import (
 	"fmt"
 	bolt "go.etcd.io/bbolt"
 	"log"
+	"strconv"
 	"strings"
 	"time"
 )
+
+func convertTime (international string) string{
+	internationalSplit := strings.Split(international, ":")
+	hour, _ := strconv.Atoi(internationalSplit[0])
+	if hour > 12{
+		return strconv.Itoa(hour - 12) + ":" + internationalSplit[1] + " PM"
+	}else{
+		return international + " AM"
+	}
+}
+
+func calcTimeWorked (in string, out string) string{
+	inSplit := strings.Split(in, ":")
+	outSplit := strings.Split(out, ":")
+	inHour, _ := strconv.Atoi(inSplit[0])
+	outHour, _ := strconv.Atoi(outSplit[0])
+	hoursWorked := outHour - inHour
+	inMinute, _ := strconv.Atoi(inSplit[1])
+	outMinute, _ := strconv.Atoi(outSplit[1])
+	// Edge case example: in and out are the same
+	if outHour - inHour == 0 && outMinute - inMinute == 0 {
+		return "0 hours & 0 minutes"
+	}
+	var minutesWorked int
+	// Edge case example = 6:27pm - 5:45 = 0:42
+	if outMinute > inMinute {
+		minutesWorked = outMinute - inMinute
+	} else {
+		hoursWorked--
+		minutesWorked = 60 - inMinute + outMinute
+	}
+	return strconv.Itoa(hoursWorked) + " hours & " + strconv.Itoa(minutesWorked) + " minutes."
+}
 
 // Record time in
 func punch(){
@@ -28,17 +62,27 @@ func punch(){
 		currentDate :=  timeDateSplit[0] // Use today's date as key
 		hoursWorked := logBucket.Get([]byte(currentDate)) // Value from bucket is hours worked
 		if(hoursWorked == nil) { // Punch in because no time has been logged for today
-			err := logBucket.Put([]byte(currentDate), []byte(currentTime))
+			err := logBucket.Put([]byte(currentDate), append([]byte(currentTime), []byte(",")...))
 			if err != nil {
 				log.Fatal(err)
 			}
-			fmt.Println("Punched in at", currentTime)
+			fmt.Print("Punched in at " + convertTime(currentTime) + ".\n")
 		} else{ // Punch out because time in was previously logged
-			err := logBucket.Put([]byte(currentDate), append(logBucket.Get([]byte(currentDate)), append([]byte(" "), []byte(currentTime)...)...))
-			if err != nil {
-				log.Fatal(err)
+			hoursWorked := logBucket.Get([]byte(currentDate))
+			hoursWorkedSplit := strings.Split(string(hoursWorked), ",")
+			if hoursWorkedSplit[1] == "" {
+				hoursWorkedSplit[1] = currentTime
+				err := logBucket.Put([]byte(currentDate), append([]byte(hoursWorked), []byte(currentTime)...))
+				if err != nil {
+					log.Fatal(err)
+				}
 			}
-			fmt.Println("Punched out at", currentTime)
+			inTime := convertTime(hoursWorkedSplit[0])
+			outTime := convertTime(hoursWorkedSplit[1])
+			fmt.Print("Punched in at " + inTime + ".\n")
+			fmt.Print("Punched out at " + outTime +  ".\n")
+			timeWorked := calcTimeWorked(inTime, outTime)
+			fmt.Print("Time worked today: " + timeWorked +  ".\n")
 		}
 
 		return nil
@@ -47,7 +91,6 @@ func punch(){
 		log.Fatal("Error: Could not log hours in database.")
 	}
 }
-
 func main() {
 	punch()
 
